8b35c6eae0da7235607bff883a023a03f0eb70fd
pond7897 <80212418+pond7897@users.noreply.github.com>
2022-11-17 00:07:21 +0700

    Add files via upload

--- POS_OOP/src/Pos_Project/Success_PayMoney.java
+++ POS_OOP/src/Pos_Project/Success_PayMoney.java
@@ -12,6 +12,7 @@ import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Success_PayMoney extends JFrame {
 
@@ -25,6 +26,7 @@ public class Success_PayMoney extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1080, 720);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(248, 209, 224));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -44,7 +46,7 @@ public class Success_PayMoney extends JFrame {
 
 		JLabel lblNewLabel_1_1 = new JLabel("ชำระเงินเสร็จสิ้น");
 		lblNewLabel_1_1.setFont(new Font("Kanit", Font.PLAIN, 80));
-		lblNewLabel_1_1.setBounds(287, 436, 527, 104);
+		lblNewLabel_1_1.setBounds(287, 436, 535, 84);
 		contentPane.add(lblNewLabel_1_1);
 
 		JButton btnNewButton = new JButton("หน้าแรก");
@@ -56,13 +58,13 @@ public class Success_PayMoney extends JFrame {
 			}
 		});
 		btnNewButton.setFont(new Font("Kanit", Font.PLAIN, 30));
-		btnNewButton.setBounds(407, 597, 236, 76);
+		btnNewButton.setBounds(410, 591, 215, 53);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(Success_PayMoney.class.getResource("/resources/checked.png")));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(343, 209, 379, 217);
+		lblNewLabel.setBounds(343, 195, 354, 200);
 		contentPane.add(lblNewLabel);
 	}
 	
